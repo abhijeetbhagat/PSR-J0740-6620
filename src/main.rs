edfb5b37accf6c3a4d3d5018be209f292574c5e0
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-08-28 17:56:42 +0530

    use cursive instead of tui-rs

--- src/main.rs
+++ src/main.rs
@@ -1,165 +1,166 @@
 use anyhow::Result;
-use crossterm::terminal::{
-    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
-};
-use crossterm::{
-    event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, MouseEventKind},
-    execute,
-};
-use tui::symbols::Marker;
-use tui::widgets::canvas::{Canvas, Rectangle};
+use cursive::traits::*;
+use cursive::views::{Button, Dialog, EditView, LinearLayout, TextView, ViewRef};
+use std::rc::Rc;
 
-use std::io::{stdout, Write};
-use tui::backend::CrosstermBackend;
-use tui::layout::{Constraint, Direction, Layout};
-use tui::style::{Color, Modifier, Style};
-use tui::text::{Span, Spans, Text};
-use tui::widgets::{Block, Borders, List, ListItem, Paragraph};
-use tui::Terminal;
-use unicode_width::UnicodeWidthStr;
-
-enum InputMode {
-    Normal,
-    Editing,
+struct Calc {
+    op1: u64,
+    op2: u64,
+    should_clear: bool,
 }
-
-struct App {
-    input_mode: InputMode,
-    input: String,
-    messages: Vec<String>,
-}
-
-impl App {}
 
 fn main() -> Result<()> {
-    let mut stdout = stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    enable_raw_mode()?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout))?;
-    terminal.clear()?;
-    terminal.hide_cursor()?;
+    let mut siv = cursive::default();
+    let mut layout = LinearLayout::vertical();
 
-    let mut app = App {
-        input: String::new(),
-        input_mode: InputMode::Normal,
-        messages: vec![],
+    let state = Calc {
+        op1: 0,
+        op2: 0,
+        should_clear: false,
     };
+    siv.set_user_data(state);
 
-    loop {
-        terminal.draw(|f| {
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(2)
-                .constraints(
-                    [
-                        Constraint::Length(1),
-                        Constraint::Length(3),
-                        Constraint::Min(1),
-                    ]
-                    .as_ref(),
-                )
-                .split(f.size());
-
-            let (msg, style) = match app.input_mode {
-                InputMode::Normal => (
-                    vec![
-                        Span::raw("Press"),
-                        Span::styled(" q", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to exit, "),
-                        Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to start editing"),
-                    ],
-                    Style::default().add_modifier(Modifier::RAPID_BLINK),
-                ),
-                InputMode::Editing => (
-                    vec![
-                        Span::raw("Press "),
-                        Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to stop editing, "),
-                        Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
-                        Span::raw(" to record the message"),
-                    ],
-                    Style::default(),
-                ),
-            };
-
-            let mut text = Text::from(Spans::from(msg));
-            text.patch_style(style);
-
-            let help_message = Paragraph::new(text);
-            f.render_widget(help_message, chunks[0]);
-
-            let input = Paragraph::new(app.input.as_ref())
-                .style(match app.input_mode {
-                    InputMode::Normal => Style::default(),
-                    InputMode::Editing => Style::default().fg(Color::Yellow),
-                })
-                .block(Block::default().borders(Borders::ALL).title("Input"));
-            f.render_widget(input, chunks[1]);
-
-            match app.input_mode {
-                InputMode::Normal => {}
-                InputMode::Editing => {
-                    f.set_cursor(chunks[1].x + app.input.width() as u16 + 1, chunks[1].y + 1)
-                }
+    let first = LinearLayout::horizontal()
+        .child(Button::new("1", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            let state = s.user_data::<Calc>().unwrap();
+            if state.should_clear {
+                input = "".into();
             }
+            input.push('1');
 
-            let numpad = Canvas::default()
-                .block(Block::default().borders(Borders::ALL).title("Numpad"))
-                .paint(|ctx| {
-                    ctx.draw(&Rectangle {
-                        x: 10.0,
-                        y: 10.0,
-                        width: 10.0,
-                        height: 10.0,
-                        color: Color::Red,
-                    });
-                    ctx.draw(&Rectangle {
-                        x: 25.0,
-                        y: 10.0,
-                        width: 10.0,
-                        height: 10.0,
-                        color: Color::Red,
-                    });
-                })
-                .x_bounds([10.0, 110.0])
-                .y_bounds([10.0, 110.0]);
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("2", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('2');
 
-            f.render_widget(numpad, chunks[2]);
-        })?;
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("3", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('3');
 
-        match read()? {
-            Event::Key(input) => match app.input_mode {
-                InputMode::Normal => match input.code {
-                    KeyCode::Char('e') => app.input_mode = InputMode::Editing,
-                    KeyCode::Char('q') => {
-                        break;
-                    }
-                    _ => {}
-                },
-                InputMode::Editing => match input.code {
-                    KeyCode::Enter => app.messages.push(app.input.drain(..).collect()),
-                    KeyCode::Char(c) => app.input.push(c),
-                    KeyCode::Backspace => {
-                        app.input.pop();
-                    }
-                    KeyCode::Esc => app.input_mode = InputMode::Normal,
-                    _ => {}
-                },
-            },
-            Event::Mouse(input) => match input.kind {
-                MouseEventKind::Down(_) => println!("{}, {}", input.column, input.row),
-                _ => {}
-            },
-            _ => {}
-        }
-    }
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        DisableMouseCapture,
-        LeaveAlternateScreen
-    )?;
-    terminal.show_cursor()?;
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("+", |s| {
+            let tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            s.with_user_data(|data: &mut Calc| {
+                data.op1 = input.parse().unwrap();
+                data.should_clear = true;
+            });
+        }));
+    let second = LinearLayout::horizontal()
+        .child(Button::new("4", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('4');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("5", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('5');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("6", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('6');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("-", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('3');
+
+            tb.set_content(input.as_str());
+        }));
+
+    let third = LinearLayout::horizontal()
+        .child(Button::new("7", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('7');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("8", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('8');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("9", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('9');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("*", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('3');
+
+            tb.set_content(input.as_str());
+        }));
+
+    let fourth = LinearLayout::horizontal()
+        .child(Button::new("0", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('0');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("/", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let mut input = input.clone();
+            input.push('3');
+
+            tb.set_content(input.as_str());
+        }))
+        .child(Button::new("=", |s| {
+            let mut tb: ViewRef<EditView> = s.find_name("input").unwrap();
+            let input = &*tb.get_content();
+            let input = input.clone();
+            s.with_user_data(|data: &mut Calc| {
+                data.op2 = input.parse().unwrap();
+                data.op1 = data.op1 + data.op2;
+                tb.set_content(&data.op1.to_string());
+            });
+        }));
+
+    layout = layout.child(EditView::new().with_name("input").fixed_width(20));
+    layout = layout.child(first);
+    layout = layout.child(second);
+    layout = layout.child(third);
+    layout = layout.child(fourth);
+
+    siv.add_layer(layout);
+    siv.add_global_callback('q', |s| s.quit());
+    siv.run();
     Ok(())
 }
